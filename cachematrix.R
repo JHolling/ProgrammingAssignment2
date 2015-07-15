08065043cdb51fed8cc9385ab47c4066a24dcc2e
JHolling <hp1mopar1@comcast.net>
2015-07-15 10:58:49 -0700

    Udpated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## calling makeCacheMatrix gets a "x" matrix and calls a function from cacheSolve
+## to return the inverted matrix of "x" if it exists. If inverted matrix doesn't exist,
+## create one from x and set it the to the cache. 
 
 makeCacheMatrix <- function(x = matrix()) {
+        invert_mat <- x$getinverse()                   #gets any cached inverted matrix and sets to local "im" 
+        if(!is.null(invert_mat))                       #checks if "im" exists or not
+        {
+                message("getting cached data") 
+                return(invert_mat)                     #leave fun cacheSolve with existing "im" inverted matrix
+        }
+        data <- x$get()            #if no "im" cached existing, call fun get() to get non-inverted x "matrix"
+        invert_mat <- solve(data, ...)     #run solve fun to Invert the matrix and place in "im" variable
+        x$setinverse(invert_mat)           #call setinverse() to set newly inverted matrix to parent "im"
+        invert_mat                         #send "im" to screen and end.
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve contains sets List & parent enviroment variables and several functions to set/get 
+#and set/get inverse. These functions are called from makeCacheMatrix 
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invert_mat <- NULL
+        set <- function(y) {                
+                x <<- y                            #assigning variable to parent
+                invert_mat <<- NULL                        #assinging variable to NULL and Parent
+        }
+        get <- function() x
+        setinverse <- function(solve) invert_mat <<- solve  #calling to invert matrix 
+        getinverse <- function() invert_mat                 # invert matrix
+        list(set = set, get = get,
+             setinverse = setinvers,
+             getinverse = getinverse)
 }
