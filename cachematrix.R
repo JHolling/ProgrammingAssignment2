cc671135a7a9314ed07184f571e6754840866fc6
JHolling <hp1mopar1@comcast.net>
2015-07-20 17:59:58 -0700

    Updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,35 @@
 ## cachematrix.R can be used to allow one inverted matrix "cached" for use
 ## on many calls and save time on not having to recalculate it.
 ##
-## The makeCacheMatrix function, gets a matrix "x", sets it to a variable, and defines
-## a special List of objects/functions to read & write the current matrix as well 
-## as read & write the existing inverted matrix (if any).
-##
-## The cacheSolve function takes the matrix from makeCacheMatrix, and uses several 
-## functions from the above Special List of Objects/functions to determing if the inverted
-## matrix already exists. If not, it will invert the "x" with an R function 
-## called "Solve" and set it as the new cached inverted matrix.
-##
-## function usasge:
+## function usage:
 ## m<-matrix(c(1,2,2,1),nrow=2,ncol=2)
 ## matrix<-makeCacheMatrix(m)
 ## cacheSolve(matrix) 
-
+##
+## The "makeCacheMatrix" function, gets a matrix "x", sets it to a variable, and defines
+## a special List of objects/functions to read & write the current matrix as well 
+## as read & write the existing inverted matrix (if any).
+ 
 makeCacheMatrix <- function(x = matrix())
-        {
+{
         invert_mat <- NULL
         set <- function(y) 
         {                
-                x <<- y                          #assigning local variable to parent variables
-                invert_mat <<- NULL               
+                x <<- y                          # assigning local variable to 
+                invert_mat <<- NULL              # parent variables
         }
         get <- function() x
         setinverse <- function(solve) invert_mat <<- solve  
         getinverse <- function() invert_mat                 
-        list(set = set, get = get,
+        list(set = set, get = get,              
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
-
-## Checks to see if inverted matrix exist. If not, sets & returns an newly inverted matrix m. 
+## The "cacheSolve" function takes the matrix from makeCacheMatrix, and uses several 
+## functions from the above Special List of Objects/functions to determing if the inverted
+## matrix already exists. If not, it will invert the "x" with an R function 
+## called "Solve" and set it as the new cached inverted matrix.
 
 cacheSolve <- function(x, ...)  
 {
